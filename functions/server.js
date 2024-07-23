f0edc491aa8d25571adb4a95c1b41fe4f09e97ad
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-07-23 12:01:06 -0300

    prueba deplooy

---
+++ functions/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import serverless from "serverless-http";
+import { getCertificates } from "./MOCK-CERTIFICATES.js";
+import { getListings } from "./MOCK-LISTINGS.js";
+
+var app = express();
+app.use(cors());
+
+var port = process.env.PORT || 5000;
+const router = express.Router();
+
+router.get("/certs", function (req, res) {
+  res.json(getCertificates());
+});
+
+router.get("/listings", function (req, res) {
+  res.json(getListings());
+});
+
+// Iniciar servidor
+app.use('/.netlify/functions/server', router);
+export const handler = serverless(app);
