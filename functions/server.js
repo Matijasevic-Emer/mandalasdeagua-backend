613425f69cfc6beb116b06e0726c8ba23c3552bf
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-08-31 11:14:01 -0300

    agrego en server

--- functions/server.js
+++ functions/server.js
@@ -2,22 +2,36 @@ import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
 import { getCertificates } from "./MOCK-CERTIFICATES.js";
-import { getListings } from "./MOCK-LISTINGS.js";
+import { getListings, getListingById } from "./MOCK-LISTINGS.js";
 
-var app = express();
+const app = express();
 app.use(cors());
 
-var port = process.env.PORT || 5000;
 const router = express.Router();
 
-router.get("/certs", function (req, res) {
+router.get("/certs", (req, res) => {
   res.json(getCertificates());
 });
 
-router.get("/listings", function (req, res) {
-  res.json(getListings());
+router.get("/listings", (req, res) => {
+  const listings = getListings();
+  const { id } = req.query;
+
+  // Verificar si hay un ID en los query params
+  if (id) {
+    const listing = getListingById(Number(id));
+    if (listing) {
+      res.json(listing);
+    } else {
+      res.status(404).json({ message: "Listing not found" });
+    }
+  } else {
+    // Si no hay ID, devolver todos los listings
+    res.json(listings);
+  }
 });
 
-// Iniciar servidor
+// Iniciar servidor en el contexto de Netlify Functions
 app.use('/.netlify/functions/server', router);
+
 export const handler = serverless(app);
